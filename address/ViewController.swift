4e87947dbcce246edb2f9960a2aafac4d3dfae66
toktaktk <toktaktk@gmail.com>
2017-05-05 10:40:14 +0700

    add json to project

--- address/ViewController.swift
+++ address/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var provinceJson : JSON!
+    var districtJson : JSON!
+    var subDistrictJson : JSON!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+//        if let path = Bundle.main.path(forResource: "changwats", ofType: "json") {
+//            do {
+//                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+//                let jsonObj = JSON(data: data)
+//                if changwatJson != JSON.null {
+//                    
+//                    changwatJson = jsonObj["th"]["changwats"]
+//                    
+//                    changwatTableView.reloadData()
+//                    
+//                    print("jsonData:\(changwatJson)")
+//                } else {
+//                    print("Could not get json from file, make sure that file contains valid json.")
+//                }
+//            } catch let error {
+//                print(error.localizedDescription)
+//            }
+//        } else {
+//            print("Invalid filename/path.")
+//        }
+    
     }
 
     override func didReceiveMemoryWarning() {
